86f419b72dccc063a4cde8ddb138a278d8ae48ce
txyang1 <tingxinyang2@gamil.com>
2025-08-04 10:37:10 +0200

    new dataset name

--- verl-intuitor/math_intuitor.sh
+++ verl-intuitor/math_intuitor.sh
@@ -6,8 +6,8 @@ export HYDRA_FULL_ERROR=1
 
 PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=intuitor \
-    data.train_files=$HOME/data/math/train.parquet \
-    data.val_files=$HOME/data/math/test.parquet \
+    data.train_files=$HOME/data/math/train_old.parquet \
+    data.val_files=$HOME/data/math/test_old.parquet \
     data.train_batch_size=64 \
     data.max_prompt_length=512 \
     data.max_response_length=3072 \
